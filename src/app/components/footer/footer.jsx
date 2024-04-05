d06fa51f1b89a4de7541bd4eb701a3289a9cfbe5
courtraee <courtney.rae@hotmail.co.uk>
2024-04-05 20:41:48 +0100

    Created footer skeleton

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -1,9 +1,22 @@
 import "react";
 import "./footer.css";
+import Image from "next/image";
+
+import logo from "../../../../images/f1-logo.png";
 
 function Footer() {
   return (
-    <footer className="footer">This is the footer.</footer>
+    <footer className="footer">
+      <div className="footer-section-one">
+      <Image src={logo} alt="F1 Logo" width={50} height={17} />
+      </div>
+      <div className="footer-section-two">
+        <p>Created By Courtney</p>
+      </div>
+      <div className="footer-section-three">
+        <p>2024</p>
+      </div>
+    </footer>
   );
 }
 
