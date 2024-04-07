a7d4ffd1298fdb7f465ee3f771e4256e7d5ee9b2
courtraee <courtney.rae@hotmail.co.uk>
2024-04-07 18:39:04 +0100

    Tidying up database

--- db/index.js
+++ db/index.js
@@ -6,16 +6,22 @@ const connectionString = process.env.DB_CONNECTION_STRING;
 
 // Check if the connection string is not defined, and if so, throw an error
 if (!connectionString) {
-  throw new Error(
-    "No DB_CONNECTION_STRING defined. Did you load in your env variables?"
+  console.error("DB_CONNECTION_STRING environment variable is not set.");
+} else {
+  // Use the connection string in your database connection logic
+  console.log(
+    "Connecting to the database with connection string:",
+    connectionString
   );
 }
 
 // Export a new instance of pg.Pool, which will be used to interact with the PostgreSQL database
-export const pool = new pg.Pool({
+const pool = new pg.Pool({
   // Pass the connection string to the pool, so it knows how to connect to your database
   connectionString,
   ssl: {
     rejectUnauthorized: false, // Important for some cloud databases, use with caution
   },
 });
+
+export default pool;
